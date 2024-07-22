2ac2decee80eca7ff5003f5051b47110f3455570
arij <m'hiri.arij@esprit.tn>
2024-07-22 10:03:05 +0100

    Tracking cars with license pates

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from ultralytics import YOLO
 import cv2
+import util
+from sort.sort import *
+from util import get_car, read_license_plate, write_csv
 
 
+#variables
+results = {}
+mot_tracker = Sort()
 # load models.\venv\Scripts\activate
 coco_model = YOLO('yolov8n.pt')
 license_plate_detector = YOLO('./model')
@@ -9,29 +15,39 @@ license_plate_detector = YOLO('./model')
 # load video
 cap = cv2.VideoCapture('C:/Users/21626/Videos/sample.mp4')
 
+vehicles = [2, 3, 5, 7]
+
 # read frames
 frame_nmr = -1
 ret = True
 while ret:
     frame_nmr += 1
     ret, frame = cap.read()
-    if ret and frame_nmr < 10:
-        
+    if ret:
+        results[frame_nmr] = {}
         # detect vehicles
         detections = coco_model(frame)[0]
-        print(detections)
-
+        detections_ = []
+        for detection in detections.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = detection
+            if int(class_id) in vehicles:
+                detections_.append([x1, y1, x2, y2, score])
       
         # track vehicles
+        track_ids = mot_tracker.update(np.asarray(detections_))
 
         # detect license plates
+        license_plates = license_plate_detector(frame)[0]
+        for license_plate in license_plates.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = license_plate
+            # assign license plate to car
+            xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate, track_ids)
 
-        # assign license plate to car
+            # crop license plate
+            license_plate_crop = frame[int(y1):int(y2), int(x1): int(x2), :]
 
-        # crop license plate
+            # process license plate
 
-        # process license plate
+            # read license plate number
 
-        # read license plate number
-
-        # write results
+            # write results
